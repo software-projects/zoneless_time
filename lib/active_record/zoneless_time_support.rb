3cede81f5477bc10120b0d149d7de883cb536983
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2011-07-19 15:01:15 +1000

    Add assignment operator to do conversion of zoneless time

--- lib/active_record/zoneless_time_support.rb
+++ lib/active_record/zoneless_time_support.rb
@@ -5,6 +5,13 @@ module ActiveRecord
         self.composed_of sym, :class_name => 'ZonelessTime::TimeWithoutZone',
           :mapping => [[sym, 'time']], :constructor => :at
         self.skip_time_zone_conversion_for_attributes << sym
+        define_method "#{sym}=" do |val|
+          if val.is_a? ZonelessTime::TimeWithoutZone
+            super val
+          else
+            super ZonelessTime::TimeWithoutZone.at(val)
+          end
+        end
       end
 
       if time_zone_aware_attributes
