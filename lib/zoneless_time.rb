223a47c242dfa952f5ec76716ba9232b3ea0a554
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2011-07-19 13:46:26 +1000

    Fixed initialization error caused by not including classes

--- lib/zoneless_time.rb
+++ lib/zoneless_time.rb
@@ -1,6 +1,9 @@
 module ZonelessTime
 end
 
+require 'zoneless_time/time_without_zone'
+require 'zoneless_time/equality_with_zoneless_time'
+
 Time.send :include, ZonelessTime::EqualityWithZonelessTime
 
 require 'zoneless_time/railtie' if defined? Rails
