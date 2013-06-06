4a3b6001b126d31a83ed0bc2b4a813c39d18d1d2
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2013-06-06 10:40:35 +1000

    Don't use ActiveRecord's `composed_of` helper, which is deprecated and will be removed

--- lib/active_record/zoneless_time_support.rb
+++ lib/active_record/zoneless_time_support.rb
@@ -2,16 +2,28 @@ module ActiveRecord
   module ZonelessTimeSupport
     def zoneless_time(sym)
       instance_eval do
-        self.composed_of sym, :class_name => 'ZonelessTime::TimeWithoutZone',
-          :mapping => [[sym, 'time']], :constructor => :at
+        define_method sym do
+          t = instance_variable_get(:"@#{sym}")
+
+          if t.nil?
+            t = ZonelessTime::TimeWithoutZone.at(read_attribute(sym.to_s))
+            instance_variable_set(:"@#{sym}", t)
+          end
+
+          t
+        end
+
         self.skip_time_zone_conversion_for_attributes << sym
+
         define_method "#{sym}=" do |val|
           if val.is_a? ZonelessTime::TimeWithoutZone
             super val
           else
             super ZonelessTime::TimeWithoutZone.at(val)
           end
-          send sym, true
+
+          instance_variable_set(:"@#{sym}", nil)
+          send sym
         end
       end
     end
